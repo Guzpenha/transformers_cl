27d33b6a25f343e261dfb9297acff389fd2638dd
Gustavo Penha <gustavopenha@wlan-145-94-149-114.wlan.tudelft.nl>
2019-08-29 11:38:20 +0200

    adding run_pacing_function.sh file

--- pytorch_transformers_cl/run_bert_train_eval_MTL.sh
+++
@@ -1,5 +0,0 @@
-sbatch --export=ALL,RANDOM_SEED=1 bert_train_eval_ms_v2_MTL.sbatch
-sbatch --export=ALL,RANDOM_SEED=2 bert_train_eval_ms_v2_MTL.sbatch
-sbatch --export=ALL,RANDOM_SEED=3 bert_train_eval_ms_v2_MTL.sbatch
-sbatch --export=ALL,RANDOM_SEED=4 bert_train_eval_ms_v2_MTL.sbatch
-sbatch --export=ALL,RANDOM_SEED=5 bert_train_eval_ms_v2_MTL.sbatch
---
+++ pytorch_transformers_cl/run_pacing_functions.sh
@@ -0,0 +1,5 @@
+sbatch --export=ALL,RANDOM_SEED=1 pacing_func_ms_v2.sbatch
+sbatch --export=ALL,RANDOM_SEED=2 pacing_func_ms_v2.sbatch
+sbatch --export=ALL,RANDOM_SEED=3 pacing_func_ms_v2.sbatch
+sbatch --export=ALL,RANDOM_SEED=4 pacing_func_ms_v2.sbatch
+sbatch --export=ALL,RANDOM_SEED=5 pacing_func_ms_v2.sbatch
