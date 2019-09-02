19101799e27fc3de5027625f93ac239c64984fd5
Gustavo Penha <gustavopenha@Gustavos-MacBook-Pro.local>
2019-09-02 22:03:38 +0200

    some fixes

--- pytorch_transformers_cl/calculate_p_values.py
+++ pytorch_transformers_cl/calculate_p_values.py
@@ -18,12 +18,12 @@ args = parser.parse_args()
 path_dataset = path+args.dataset+"_output/"
 final_str = ""
 for seed in ['1','2','3','4','5']:
-	baseline = pd.read_csv(path_dataset+'aps_run_cl__random_c_3_seed_'+seed, names=['ap'])
+	baseline = pd.read_csv(path_dataset+'aps_run_cl__random_c_3root_5_seed_'+seed, names=['ap'])
 	final_str += str(baseline.mean().values[0])+ "\t"
-	for competing_scoring in ['_turns_c_3', '_utt_avg_words_c_3',\
-						'_cand_docs_avg_words_c_3', 'max_dif_exact_match_q_d_c_3',\
-						'max_dif_semantic_match_q_d_c_3', '_bert_avg_pred_scores_c_3',\
-						'_bert_avg_loss_c_3']:
+	for competing_scoring in ['_turns_c_3root_5', '_utt_avg_words_c_3root_5',\
+						'_cand_docs_avg_words_c_3root_5', 'max_dif_exact_match_q_d_c_3root_5',\
+						'max_dif_semantic_match_q_d_c_3root_5', '_bert_avg_pred_scores_c_3root_5',\
+						'_bert_avg_loss_c_3root_5']:
 		competing = pd.read_csv(path_dataset+'aps_run_cl_'+competing_scoring+'_seed_'+seed, names=['ap'])		
 		# statistic, pvalue = stats.ttest_rel(competing['ap'].values, baseline['ap'].values)
 		statistic, pvalue = stats.wilcoxon(competing['ap'].values, baseline['ap'].values)
