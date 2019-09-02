c9fa21904561e644ab90355c0a3c5ffb23a6b50c
Gustavo Penha <gustavopenha@wlan-145-94-149-114.wlan.tudelft.nl>
2019-09-02 15:33:58 +0200

    changing n_gpu to 1

--- pytorch_transformers_cl/run_pacing_functions.sh
+++ pytorch_transformers_cl/run_pacing_functions.sh
@@ -4,8 +4,8 @@ sbatch --export=ALL,RANDOM_SEED=3 pacing_func_ms_v2.sbatch
 sbatch --export=ALL,RANDOM_SEED=4 pacing_func_ms_v2.sbatch
 sbatch --export=ALL,RANDOM_SEED=5 pacing_func_ms_v2.sbatch
 
-sbatch --export=ALL,RANDOM_SEED=1 pacing_func_mantis_10.sbatch
-sbatch --export=ALL,RANDOM_SEED=2 pacing_func_mantis_10.sbatch
-sbatch --export=ALL,RANDOM_SEED=3 pacing_func_mantis_10.sbatch
-sbatch --export=ALL,RANDOM_SEED=4 pacing_func_mantis_10.sbatch
-sbatch --export=ALL,RANDOM_SEED=5 pacing_func_mantis_10.sbatch
+# sbatch --export=ALL,RANDOM_SEED=1 pacing_func_mantis_10.sbatch
+# sbatch --export=ALL,RANDOM_SEED=2 pacing_func_mantis_10.sbatch
+# sbatch --export=ALL,RANDOM_SEED=3 pacing_func_mantis_10.sbatch
+# sbatch --export=ALL,RANDOM_SEED=4 pacing_func_mantis_10.sbatch
+# sbatch --export=ALL,RANDOM_SEED=5 pacing_func_mantis_10.sbatch
