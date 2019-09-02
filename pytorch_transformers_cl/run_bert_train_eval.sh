9892e2b13ba340612a96db460b030df7692ff615
Gustavo Penha <gustavopenha@wlan-145-94-149-114.wlan.tudelft.nl>
2019-09-02 11:44:45 +0200

    adding mantis_10 sbatches

--- pytorch_transformers_cl/run_bert_train_eval.sh
+++ pytorch_transformers_cl/run_bert_train_eval.sh
@@ -3,3 +3,9 @@ sbatch --export=ALL,RANDOM_SEED=2 bert_train_eval_ms_v2.sbatch
 sbatch --export=ALL,RANDOM_SEED=3 bert_train_eval_ms_v2.sbatch
 sbatch --export=ALL,RANDOM_SEED=4 bert_train_eval_ms_v2.sbatch
 sbatch --export=ALL,RANDOM_SEED=5 bert_train_eval_ms_v2.sbatch
+
+sbatch --export=ALL,RANDOM_SEED=1 bert_train_eval_mantis_10.sbatch
+sbatch --export=ALL,RANDOM_SEED=2 bert_train_eval_mantis_10.sbatch
+sbatch --export=ALL,RANDOM_SEED=3 bert_train_eval_mantis_10.sbatch
+sbatch --export=ALL,RANDOM_SEED=4 bert_train_eval_mantis_10.sbatch
+sbatch --export=ALL,RANDOM_SEED=5 bert_train_eval_mantis_10.sbatch
